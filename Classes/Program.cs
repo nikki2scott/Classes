e6ed9f97bf2c8f5e041412ca268d6818b955d6ef
nikki2scott <nscott2nd04@gmail.com>
2025-06-03 16:33:20 -0700

    create .net project

---
+++ Classes/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("What is your favorite season?");
+string? userInput = Console.ReadLine();
+
+switch (userInput.ToLower())
+{
+    case "summer":
+        Console.WriteLine("Wow! I love summer too!");
+        break;
+    case "spring":
+        Console.WriteLine("Spring is also my favorite");
+        break;
+    case "winter":
+        Console.WriteLine("Winter is my least favorite");
+        break;
+    case "fall":
+        Console.WriteLine("The falling leaves are so pretty");
+        break;
+    default:
+        Console.WriteLine("What was that season? I didn't catch it");
+        break;
+}
